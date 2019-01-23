ab802e48eddc355efa0d291c8446c82e06156caf
IainMu <35377396+IainMu@users.noreply.github.com>
2019-01-23 21:31:32 +0000

    Most of previous work
    Needing to upload library but too many files for one upload

---
+++ Assets/BarcodeReader.cs
@@ -0,0 +1,50 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ZXing;
+using ZXing.QrCode;
+using System;
+
+
+public class BarcodeReader : MonoBehaviour {
+
+
+
+private WebCamTexture camTexture;
+private Rect screenRect;
+void Start() {
+  screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        camTexture = new WebCamTexture
+        {
+            requestedHeight = Screen.height,
+            requestedWidth = Screen.width
+        };
+        if (camTexture != null) {
+    camTexture.Play();
+  }
+}
+
+void Update(){
+        if (Screen.orientation != ScreenOrientation.LandscapeLeft)
+        {
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+        }
+    }
+    void OnGUI () {
+  // drawing the camera on screen
+  GUI.DrawTexture (screenRect, camTexture, ScaleMode.ScaleToFit);
+  // do the reading — you might want to attempt to read less often than you draw on the screen for performance sake
+  try {
+            IBarcodeReader barcodeReader = (IBarcodeReader) new BarcodeReader();
+            // decode the current frame
+            var result = barcodeReader.Decode(camTexture.GetPixels32(),
+      camTexture.width, camTexture.height);
+    if (result != null) {
+                GUI.Box(new Rect(Screen.width/2-20, Screen.height / 2 - 20,40,40),Decode.decode(3,result.Text));
+    }
+  } catch(Exception ex) { Debug.LogWarning (ex.Message); }
+}
+}
+*/
---
+++ Assets/UpdateW.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateW : MonoBehaviour {
+
+ 
+    static Text value;
+	// Use this for initialization
+	void Start () {
+       value = gameObject.GetComponent<Text>();
+	}
+
+    public static void setText(string val)
+    {
+        value.text = val;
+    }
+}
